02a2f4180da62394a71f329a17639213d6e16f01
lolrandomxd <taoufiqnaciri234@gmail.com>
2024-04-28 15:17:13 +0100

    update user and session db schema

--- server/db/schema/user.ts
+++ server/db/schema/user.ts
@@ -1,5 +1,10 @@
 import { pgTable, text } from "drizzle-orm/pg-core";
 
 export const userTable = pgTable("user", {
-	id: text("id").primaryKey()
+  id: text("id").primaryKey().notNull(),
+  facebookId: text("facebook_id").unique().notNull(),
+  username: text("username").notNull(),
 });
+
+export type UserInsert = typeof userTable.$inferInsert;
+export type UserSelect = typeof userTable.$inferSelect;
