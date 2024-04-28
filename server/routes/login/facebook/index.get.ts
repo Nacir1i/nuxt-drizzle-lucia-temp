31eb863d043d7f4eaead23eb3a256b9a5e645cb7
lolrandomxd <taoufiqnaciri234@gmail.com>
2024-04-28 15:19:00 +0100

    create login server route

---
+++ server/routes/login/facebook/index.get.ts
@@ -0,0 +1,17 @@
+import { generateState } from "arctic";
+import { facebook } from "~/server/utils/auth";
+
+export default defineEventHandler(async (event) => {
+  const state = generateState();
+  const url = await facebook.createAuthorizationURL(state);
+
+  setCookie(event, "facebook_oauth_state", state, {
+    path: "/",
+    secure: process.env.NODE_ENV === "production",
+    httpOnly: true,
+    maxAge: 60 * 10,
+    sameSite: "lax",
+  });
+
+  return sendRedirect(event, url.toString());
+});
