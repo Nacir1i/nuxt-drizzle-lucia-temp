ae76d865abbd9b11159ae0c3a9b32ec1be8e884f
lolrandomxd <taoufiqnaciri234@gmail.com>
2024-04-28 16:07:15 +0100

    fix facebook login button

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,4 +1,6 @@
 <template>
 	<h1 class="text-2xl text-red-500">Sign in</h1>
-	<Button variant="link" size="lg" href="/login/facebook">Sign in with Facebook</Button>
+	<Button variant="link" size="lg">
+		<a href="/login/facebook">Sign in with Facebook</a>
+	</Button>
 </template>
